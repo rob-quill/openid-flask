4fb8b562ffd091d52ff5a51c118aa581f821cfbe
rob-quill <andersonmatheos1031@gmail.com>
2026-09-20 13:53:54 -0500

    init

---
+++ README.rst
@@ -0,0 +1,24 @@
+Flask-OpenID
+============
+
+Adds support for OpenID to flask applications.  Check out the
+example for more information.
+
+Documentation: http://packages.python.org/Flask-OpenID/
+
+Note that this library is for a very old and outdated version of OpenID.
+Almost no-one uses this Open ID protocol, the OpenID Connect protocol is
+the current standard.
+
+CHANGELOG
+---------
+
+* 1.3.1 Version: 26, May 2024
+
+Import quote from urllib.
+
+
+* 1.3.0 Version: 8, September 2021
+
+Python 3-only compatible version with 2to3 removed to satisfy
+setuptools removal of 2to3 configuration option of setup.py
---
+++ setup.py
@@ -0,0 +1,55 @@
+"""
+Flask-OpenID
+============
+
+Adds OpenID support to Flask.
+
+Links:
+
+* `Flask-OpenID Documentation <http://packages.python.org/Flask-OpenID/>`_
+* `Flask <http://flask.pocoo.org>`_
+* `development version
+  <http://github.com/mitsuhiko/flask-openid/zipball/master#egg=Flask-OpenID-dev>`_
+"""
+from setuptools import setup
+import sys
+import os
+
+# This check is to make sure we checkout docs/_themes before running sdist
+if not os.path.exists("./docs/_themes/README"):
+    print('Please make sure you have docs/_themes checked out while running setup.py!')
+    if os.path.exists('.git'):
+        print('You seem to be using a git checkout, please execute the following commands to get the docs/_themes directory:')
+        print(' - git submodule init')
+        print(' - git submodule update')
+    else:
+        print('You seem to be using a release. Please use the release tarball from PyPI instead of the archive from GitHub')
+    sys.exit(1)
+
+setup(
+    name='Flask-OpenID',
+    version='1.3.1',
+    url='http://github.com/mitsuhiko/flask-openid/',
+    license='BSD',
+    author='Armin Ronacher, Patrick Uiterwijk, Jarek Potiuk, Jason R. Coombs, Emmanuel Bavoux',
+    author_email='armin.ronacher@active-4.com, puiterwijk@redhat.com, jarek@potiuk.com, '
+                 'jaraco@jaraco.com, emmanuel.bavoux@free2move.com',
+    description='OpenID support for Flask',
+    py_modules=['flask_openid'],
+    zip_safe=False,
+    platforms='any',
+    install_requires=['Flask>=0.10.1', 'python3-openid>=2.0'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    python_requires=">=3.0",
+)
